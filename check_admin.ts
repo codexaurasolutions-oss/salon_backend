4efcaa07acb26bb4d7927a0e0bfb1ef9b81a1ada
Ahmed Bilal Khan <huntergaming5555566@gmail.com>
2026-06-23 01:40:11 +0500

    chore: clean up temporary build and test files for delivery

--- check_admin.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const p = new PrismaClient();
-p.user.findUnique({where:{email:'test@admin.com'}, include: {profile: true, admin: true}})
-.then(data => console.log(JSON.stringify(data, null, 2)))
-.catch(console.error)
-.finally(() => p.$disconnect());
